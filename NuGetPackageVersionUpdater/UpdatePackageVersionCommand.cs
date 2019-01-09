fc4602b8d01ab8c3dac9462d986332441a7a965c
Brett <digitalrizzle@gmail.com>
2019-01-09 14:27:17 +1300

    Add project files.

---
+++ NuGetPackageVersionUpdater/UpdatePackageVersionCommand.cs
@@ -0,0 +1,144 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Text;
+using System.Text.RegularExpressions;
+using Task = System.Threading.Tasks.Task;
+
+namespace NuGetPackageVersionUpdater
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class UpdatePackageVersionCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("efb83abd-934c-4f60-a8cb-833a58f545ab");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UpdatePackageVersionCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private UpdatePackageVersionCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static UpdatePackageVersionCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in UpdatePackageVersionCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new UpdatePackageVersionCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            int successful = 0;
+            var builder = new StringBuilder();
+            var dte = (DTE)Package.GetGlobalService(typeof(DTE));
+            var selected = dte.ActiveSolutionProjects as object[];
+
+            foreach (Project project in selected)
+            {
+                var version = (string)project.Properties.Item("Version")?.Value;
+                var incremented = IncrementVersion(version);
+
+                if (incremented != null)
+                {
+                    project.Properties.Item("Version").Value = incremented;
+                    project.Save();
+
+                    successful++;
+                    builder.AppendLine($"{project.Name}: {version} → {incremented}");
+                }
+            }
+
+            var title = $"NuGet Package Version Update{Environment.NewLine}{successful}/{selected.Length} updated";
+            var message = builder.ToString();
+
+            VsShellUtilities.ShowMessageBox(
+                this.package,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+
+        private string IncrementVersion(string input)
+        {
+            var match = Regex.Match(input, @"\d+$");
+
+            if (match.Success)
+            {
+                if (int.TryParse(match.Value, out int result))
+                {
+                    var output = Regex.Replace(input, @"\d+$", $"{result + 1}");
+
+                    return output;
+                }
+
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
